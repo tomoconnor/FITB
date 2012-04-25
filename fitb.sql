5dba20e7def29218772c0dd685ecafbbd2f42f86
Tom O'Connor <tom.oconnor@baseblack.com>
2012-04-25 15:42:22 +0100

    so postgres doesn't support unsigned stuff

--- fitb.sql
+++ fitb.sql
@@ -5,7 +5,7 @@ CREATE TABLE "ports" (
   "filename" varchar(255) NOT NULL,
   "alias" text NOT NULL,
   "graphtype" varchar(255) NOT NULL,
-  "lastpoll" integer unsigned NOT NULL,
+  "lastpoll" integer NOT NULL,
   PRIMARY KEY  ("filename"),
   KEY "host_name" ("host"),
   KEY "port_name" ("name")
